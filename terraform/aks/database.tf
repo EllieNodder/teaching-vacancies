96e08a7c7c51aa3b7a5f16e00c3115e540906b9b
bolaji jibodu <bolaji.jibodu@education.gov.uk>
2023-08-03 12:50:53 +0100

    Deploy application

--- terraform/aks/database.tf
+++ terraform/aks/database.tf
@@ -10,22 +10,22 @@ module "postgres" {
   cluster_configuration_map   = module.cluster_data.configuration_map
   use_azure                   = var.deploy_azure_backing_services
   azure_enable_backup_storage = var.azure_enable_backup_storage
-  azure_enable_monitoring      = var.enable_monitoring
-  server_version               = "14"
-  azure_extensions             = ["POSTGIS", "address_standardizer", "plpgsql"]
+  azure_enable_monitoring     = var.enable_monitoring
+  server_version              = "14"
+  azure_extensions            = ["POSTGIS", "address_standardizer", "plpgsql"]
 }
 module "redis-cache" {
-  source                       = "./vendor/modules/aks//aks/redis"
+  source = "./vendor/modules/aks//aks/redis"
 
-  namespace                    = var.namespace
-  environment                  = var.environment
-  azure_resource_prefix        = var.azure_resource_prefix
-  service_short                = var.service_short
-  config_short                 = var.config_short
-  service_name                 = var.service_name
-  cluster_configuration_map    = module.cluster_data.configuration_map
-  use_azure                    = var.deploy_azure_backing_services
-  azure_enable_monitoring      = var.enable_monitoring
-  azure_patch_schedule         = [{ "day_of_week": "Sunday", "start_hour_utc": 01 }]
-  azure_maxmemory_policy       = "allkeys-lfu"
+  namespace                 = var.namespace
+  environment               = var.environment
+  azure_resource_prefix     = var.azure_resource_prefix
+  service_short             = var.service_short
+  config_short              = var.config_short
+  service_name              = var.service_name
+  cluster_configuration_map = module.cluster_data.configuration_map
+  use_azure                 = var.deploy_azure_backing_services
+  azure_enable_monitoring   = var.enable_monitoring
+  azure_patch_schedule      = [{ "day_of_week" : "Sunday", "start_hour_utc" : 01 }]
+  azure_maxmemory_policy    = "allkeys-lfu"
 }
