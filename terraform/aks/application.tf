fb7d2334da5e8f6d696a5eba2c335784f37c8494
bolaji jibodu <bolaji.jibodu@education.gov.uk>
2023-08-22 08:04:11 +0100

    application deployment

---
+++ terraform/aks/application.tf
@@ -0,0 +1,50 @@
+module "application_configuration" {
+  source = "./vendor/modules/aks//aks/application_configuration"
+
+  namespace              = var.namespace
+  environment            = var.environment
+  azure_resource_prefix  = var.azure_resource_prefix
+  service_short          = var.service_short
+  config_short           = var.config_short
+  secret_key_vault_short = "app"
+
+
+  # Delete for non rails apps
+  is_rails_application = true
+
+  config_variables = merge(
+    /* local.app_environment = merge */
+    local.app_env_values,
+    {
+      ENVIRONMENT_NAME = var.environment
+      PGSSLMODE        = local.postgres_ssl_mode
+      DOMAIN = "teaching-vacancies-${var.environment}.${module.cluster_data.ingress_domain}"
+  })
+  secret_variables = merge(
+    yamldecode(data.aws_ssm_parameter.app_env_api_key_big_query.value),
+    yamldecode(data.aws_ssm_parameter.app_env_api_key_google.value),
+    yamldecode(data.aws_ssm_parameter.app_env_secrets.value),
+    {
+      DATABASE_URL = module.postgres.url
+      REDIS_URL    = module.redis-cache.url
+  })
+}
+
+module "web_application" {
+  source = "./vendor/modules/aks//aks/application"
+
+  is_web = true
+
+  namespace    = var.namespace
+  environment  = var.environment
+  service_name = var.service_name
+
+  /* cluster_configuration_map  = module.cluster_data.configuration_map */
+  kubernetes_config_map_name = module.application_configuration.kubernetes_config_map_name
+  kubernetes_secret_name     = module.application_configuration.kubernetes_secret_name
+
+  docker_image = var.docker_image
+  command      = var.web_app_start_command
+  probe_path   = null
+
+}
--- terraform/app/terraform.tf
+++ terraform/app/terraform.tf
@@ -24,13 +24,13 @@ Store infrastructure state in a remote store (instead of local machine):
 https://www.terraform.io/docs/state/purpose.html
 */
 terraform {
-
-  backend "s3" {
+     backend "s3" {
     bucket = "530003481352-terraform-state"
     # When run interactively, should prompt for key. To specify environments, we pass to terraform init -backend-config="key=dev/app.tfstate"
     region  = "eu-west-2"
     encrypt = "true"
   }
+
 }
 
 module "cloudfront" {
